1d92d9e9497b5b29a0c10572c3b926929d858358
saiwas <johnnysaiwas@gmail.com>
2021-12-26 15:08:07 +0000

    Deploying to gh-pages from @ saiwas/saiwas.github.io@06b60ea81ed36faca1784c32d1fc3771cc68cc1f 🚀

--- _next/static/2F-9Lr-F2IUWt2z5vk2Wp/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-6b8d5146da282f2e.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
